51baac4fb74b6a1db673177946ba54eb8cd86b45
Andrey Myagkov <andreyoren@gmail.com>
2021-10-27 16:55:35 +0500

    Адаптация: новинки, поиск, каталог

--- src/template/js/sura.js
+++ src/template/js/sura.js
@@ -5,3 +5,13 @@ const mp = new MobilePanel({
     'navbar': '.nav-main'
 });
 
+/**
+ * Сворачивание секций фильтра в каталоге
+ */
+$('.js-filter-as__toggle-section').on('click', function() {
+    const parent = $(this).parent();
+    parent.toggleClass('opened');
+    parent.find('.filter-as__list').slideToggle();
+    
+})
+
